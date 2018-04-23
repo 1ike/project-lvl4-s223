f773e5a6556e2db24bb38ec9d67009d1099cfa9d
1ike <googlohren@gmail.com>
2018-04-23 22:06:53 +0300

    Enhanced chat height policy.

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import cn from 'classnames';
+import $ from 'jquery';
 
 import _connect from '../../connect';
 
@@ -15,12 +16,29 @@ const mapStateToProps = (state) => {
 
 @_connect(mapStateToProps)
 export default class Messages extends React.Component {
+  setHeight() {
+    const { el } = this;
+    const scrollHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight,
+    );
+    const diff = scrollHeight - window.innerHeight;
+
+    const elInitHeight = el.offsetHeight;
+    const height = diff > 0 ? elInitHeight - diff : elInitHeight;
+    el.style.height = `${height}px`;
+  }
+
   scrollToBottom() {
     this.el.scrollTop = this.el.scrollHeight;
   }
 
   componentDidMount() {
+    this.setHeight();
     this.scrollToBottom();
+
+    $(window).resize(this.setHeight);
   }
 
   componentDidUpdate() {
